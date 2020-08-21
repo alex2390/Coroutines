9698bd1100071dffddf1096fa6c23b232507f23d
renjiawen <renjiawen@isoubu.com>
2020-08-21 17:59:05 +0800

    commit

---
+++ app/src/main/java/com/example/coroutines/rxjava/RxActivity.java
@@ -0,0 +1,30 @@
+package com.example.coroutines.rxjava;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.coroutines.R;
+
+import java.util.Observable;
+
+/**
+ * Des :
+ * <p>
+ * Date: 2020/8/21
+ * Time: 17:12
+ * author: renjiawen
+ **/
+public class RxActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
